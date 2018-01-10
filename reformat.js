36589cc5f8a4c5fa6e1accabdce4b4ac7c959d57
Jahn <ROS_eyjahn@seattleschools.org>
2018-01-10 12:35:30 -0800

    cancel button

--- reformat.js
+++ reformat.js
@@ -237,6 +237,7 @@ function isNumber(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
 
+
 function addNewScore() {
 
   // get score from getScore 
@@ -268,19 +269,21 @@ function addNewScore() {
       // add li to popup.html
       document.getElementById("myUL").appendChild(li);
 
-        // var span = document.createElement("SPAN");
-    // var txt = document.createTextNode("\u00D7");
-    // span.className = "close";
-    // span.appendChild(txt);
-    // li.appendChild(span);
+
+      var close = document.getElementsByClassName("close");
+      var span = document.createElement("SPAN");
+      var txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
 
 
-    // for (i = 0; i < close.length; i++) {
-    //   close[i].onclick = function() {
-    //     var div = this.parentElement;
-    //     div.style.display = "none";
-    //   }
-    // }
+      for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+          var div = this.parentElement;
+          div.style.display = "none";
+        }
+      }
 
       // clean up the form 
       cleanUp();
@@ -300,6 +303,7 @@ function addNewScore() {
 function addCategory() {
 
 }
+
 
 
 document.addEventListener('DOMContentLoaded', function() {
